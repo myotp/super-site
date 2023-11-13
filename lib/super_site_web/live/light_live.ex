476e721562262bd29268125bcea569077e60ddb7
jia.wang <wj2016@gmail.com>
2023-11-13 11:36:59 +0100

    [LiveView] 指明自定义layout方法从而包含main结构以便跟例子完整一致

--- lib/super_site_web/live/light_live.ex
+++ lib/super_site_web/live/light_live.ex
@@ -2,7 +2,9 @@ defmodule SuperSiteWeb.LightLive do
   # 不调用默认的layout
   # https://elixirforum.com/t/liveview-socket-put-root-layout-overwrite-root-layout/48878/2
   # 以及更现代Phoenix 1.7的写法
-  use Phoenix.LiveView, layout: false
+  # https://hexdocs.pm/phoenix_live_view/live-layouts.html#application-layout
+  # See the def controller and def live_view definitions in your MyAppWeb to learn how it is included.
+  use Phoenix.LiveView, layout: {SuperSiteWeb.Layouts, :only_main}
 
   def mount(params, session, socket) do
     IO.inspect({params, session})
