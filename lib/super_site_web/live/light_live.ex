c6fdf8f0c3c98c42b4150f2a2a5a4128b01531d1
jia.wang <wj2016@gmail.com>
2023-11-13 11:37:38 +0100

    第一个灯泡控制的例子

--- lib/super_site_web/live/light_live.ex
+++ lib/super_site_web/live/light_live.ex
@@ -6,19 +6,53 @@ defmodule SuperSiteWeb.LightLive do
   # See the def controller and def live_view definitions in your MyAppWeb to learn how it is included.
   use Phoenix.LiveView, layout: {SuperSiteWeb.Layouts, :only_main}
 
-  def mount(params, session, socket) do
-    IO.inspect({params, session})
-
-    socket =
-      assign(socket, user: "wang jia")
-
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <h1>Welcome <%= @user %></h1>
-    <h2>Some H2</h2>
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+          <%= @brightness %>%
+        </span>
+      </div>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" />
+      </button>
+      <button phx-click="down">
+        <img src="/images/down.svg" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" />
+      </button>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" />
+      </button>
+    </div>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, brightness: 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, brightness: 0)
+    {:noreply, socket}
   end
 end
